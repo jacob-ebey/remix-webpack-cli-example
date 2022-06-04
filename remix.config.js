ccf60e802f3f53859efad2793feaf61deb8f537c
Jacob Ebey <jacob.ebey@live.com>
2022-06-04 02:22:51 -0700

    initial commit

---
+++ remix.config.js
@@ -0,0 +1,16 @@
+import { BundleStatsWebpackPlugin } from "bundle-stats-webpack-plugin";
+import { DuplicatesPlugin } from "inspectpack/plugin/index.js";
+
+/** @type {Partial<import('remix-webpack-cli/lib/config-types').RemixWebpackConfig>} */
+let config = {
+  serverBuildPath: "build/index.cjs",
+  webpack: (config, { buildFor, mode }) => {
+    if (buildFor === "client" && mode === "production") {
+      config.plugins = config.plugins || [];
+      config.plugins.push(new DuplicatesPlugin({ verbose: true }));
+      config.plugins.push(new BundleStatsWebpackPlugin());
+    }
+  },
+};
+
+export default config;
